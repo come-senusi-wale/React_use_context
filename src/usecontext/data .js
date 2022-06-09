43ef525bde51e51b1bd8f869612c37a567ea121b
Akinyemi Saheed Akinwale <akinyemisaheedwale@gmail.con>
2022-06-09 13:40:41 -0700

    finish

---
+++ src/usecontext/data .js
@@ -0,0 +1,8 @@
+export let data = [
+    {id: 1, name: 'wale'},
+    {id: 2, name: 'saheed'},
+    {id: 3, name: 'tafiq'},
+    {id: 4, name: 'ayo'},
+    {id: 5, name: 'dayo'},
+    {id: 6, name: 'jamiu'}
+]
---
+++ src/usecontext/usecontext.js
@@ -0,0 +1,60 @@
+ import React, {useState, useContext}from 'react';
+
+ import { data } from "./data ";
+
+ let personContext = React.createContext();
+
+ // two context are provider and consumer ::::::::::::
+
+const Usecontext = () => {
+
+    let [people, setPeople] = useState(data);
+
+    let removehandler = (id) => {
+
+        let remainingPeople = people.filter((person) => person.id != id);
+        setPeople(remainingPeople);
+
+    }
+
+  return (
+    <personContext.Provider value={{people, removehandler}}>
+        <Loop />
+    </personContext.Provider>
+  )
+}
+
+let Loop = () => {
+
+    let {people} = useContext(personContext);
+
+    return(
+        people.map((person) => {
+            return <Single key={person.id} name={person.name} id={person.id}/>
+        })
+    )
+    
+}
+
+
+let Single = ({name, id}) => {
+
+    let {removehandler} = useContext(personContext);
+    
+
+    
+
+    return(
+        <>
+
+        <div>
+            <span>{name}</span>
+            <button type='button' onClick={() => removehandler(id)}>remove</button>
+        </div>
+            
+        </>
+    )
+
+}
+
+export default Usecontext
